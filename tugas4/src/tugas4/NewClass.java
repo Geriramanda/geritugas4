f749c68dfa44ecfa5949b3260c79a5e188378772
geriramanda <geroyramoy2@gmail.com>
2015-11-04 12:29:04 +0700

    geriramanada
    tugas4

---
+++ tugas4/src/tugas4/NewClass.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas4;
+
+/**
+ *
+ * @author owner
+ */
+public class NewClass {
+    private String penjumlahan;
+ int a;
+ int b;
+
+    public int getPenjumlahan(int penjumlahan) {
+        penjumlahan=a+b;
+        return penjumlahan=a+b;
+    }
+
+    public void setPenjumlahan(String penjumlahan) {
+        
+        this.penjumlahan =penjumlahan ;
+    }
+
+    
+}
